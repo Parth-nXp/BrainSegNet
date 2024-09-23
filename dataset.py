e16ac642415298fdacfe0e267bde274744a4938e
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2024-09-23 16:33:52 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,31 +1,42 @@
 import torch
-from torch.utils.data import Dataset
 import cv2
 import numpy as np
+from torch.utils.data import Dataset
+from PIL import Image
 
 class BrainMRIDataset(Dataset):
-    def __init__(self, image_paths, mask_paths, transform=None, device='cpu'):
-        self.image_paths = image_paths
-        self.mask_paths = mask_paths
-        self.transform = transform
-        self.device = device
-
+    def __init__(self, dataframe, image_transform=None, mask_transform=None, target_size=(256, 256)):
+        self.dataframe = dataframe
+        self.image_transform = image_transform
+        self.mask_transform = mask_transform
+        self.target_size = target_size
+        
     def __len__(self):
-        return len(self.image_paths)
-
+        return len(self.dataframe)
+    
     def __getitem__(self, idx):
-        image = cv2.imread(self.image_paths[idx])
-        image = cv2.resize(image, (256, 256))
+        image_path = self.dataframe.iloc[idx]['image_filename']
+        image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = np.transpose(image, (2, 0, 1))  # Change to channel first
-        image = image / 255.0  # Normalize
-
-        mask = cv2.imread(self.mask_paths[idx], cv2.IMREAD_GRAYSCALE)
-        mask = cv2.resize(mask, (256, 256))
-        mask = np.expand_dims(mask, axis=0)  # Add channel dimension
-        mask = mask / 255.0  # Normalize to [0,1]
-
-        image = torch.tensor(image, dtype=torch.float32).to(self.device)
-        mask = torch.tensor(mask, dtype=torch.float32).to(self.device)
-
+        
+        mask_path = self.dataframe.iloc[idx]['mask_images']
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        
+        image = cv2.resize(image, self.target_size)
+        mask = cv2.resize(mask, self.target_size)
+        
+        image = image.astype(np.float32) / 255.0
+        mask = mask.astype(np.float32) / 255.0
+        
+        image = Image.fromarray((image * 255).astype(np.uint8))
+        mask = Image.fromarray((mask * 255).astype(np.uint8))
+        
+        if self.image_transform:
+            image = self.image_transform(image)
+            
+        if self.mask_transform:
+            mask = self.mask_transform(mask)
+            
+        mask = mask.unsqueeze(0)
+        
         return image, mask
