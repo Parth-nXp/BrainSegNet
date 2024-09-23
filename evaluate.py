7f40acc7c99dbb3eeb0f22f69937b937e24a757c
Parth Sharma <86831956+Parth-nXp@users.noreply.github.com>
2024-09-23 16:34:38 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,18 +1,20 @@
-import torch
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+import torch
 
-def evaluate_model(model, dataloader, device):
+def evaluate_model(model, df_test, device, im_height, im_width):
     model.eval()
-    with torch.no_grad():
-        for images, masks in dataloader:
-            images, masks = images.to(device), masks.to(device)
-            outputs = model(images)
-            pred_masks = (outputs > 0.5).float()
-
-            plt.subplot(1, 3, 1)
-            plt.imshow(images[0].cpu().permute(1, 2, 0))
-            plt.subplot(1, 3, 2)
-            plt.imshow(masks[0].cpu().squeeze(), cmap='gray')
-            plt.subplot(1, 3, 3)
-            plt.imshow(pred_masks[0].cpu().squeeze(), cmap='gray')
-            plt.show()
+    
+    for i in range(5):
+        index = np.random.randint(0, len(df_test))
+        img_path = df_test.iloc[index]['image_filename']
+        mask_path = df_test.iloc[index]['mask_images']
+        
+        img = cv2.imread(img_path)
+        img = cv2.resize(img, (im_height, im_width))
+        img = img.astype(np.float32) / 255.0
+        img_tensor = torch.from_numpy(img).permute(2, 0, 1).unsqueeze(0).to(device)
+        
+        with torch.no_grad():
+            pred
